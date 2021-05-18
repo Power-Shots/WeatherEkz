88aedcfae66d08165964d4754c36a17592055a40
Alex <powershot317@gmail.com>
2021-05-18 17:56:26 +0300

    Done today weather and switch theam

--- js/script.js
+++ js/script.js
@@ -1,11 +1,35 @@
+const style = document.querySelector('#style');
+
 const searchCityInput = document.querySelector('#searchCity'),
       searchCityBtn = document.querySelector('#searchBtn');
 
 const main = document.querySelector('main'),
-      switchBlock = main.querySelector('#switchBlock');
+      switchBlock = main.querySelector('#switchBlock'),
+      switchTheamBlock = main.querySelector('#switchTheam');
+
+let coords = [];
+
+const serverLocks = {
+    today: {
+        currentForCity: 'https://api.openweathermap.org/data/2.5/weather?q=${searchCityInput.value}&lang=en&units=metric&appid=2e45c48feaeca4beaf24076750d9e0c7',
+        currentForCoords: 'https://api.openweathermap.org/data/2.5/weather?lat=${coords[0]}&lon=${coords[1]}&lang=en&units=metric&appid=2e45c48feaeca4beaf24076750d9e0c7',
+    }
+}
 
 const regEx = {
-    city: /^[a-zA-Z-, ]{3,100}$/
+    cityByName: /^[a-zA-Z-,' ]{3,100}$/,
+    // cityByCoords: /^-?[0-9]{1,2).[0-9]{1,},\-?\[0]$/
+}
+const dateOptions = {
+    currentDate: {
+        month: '2-digit',
+        year: 'numeric',
+        day: '2-digit', 
+    },
+    time: {
+        hour: '2-digit',
+        minute: '2-digit'
+    }
 }
 
 const fetchAsync = async (url) => {
@@ -18,9 +42,71 @@ const fetchAsync = async (url) => {
     }
 };
 
+function transormUNIX(date, option="add"){
+    if(option ==='add'){
+        return new Date(date*1000-10800000).toLocaleString('ru', dateOptions.time);
+    }
+    else{
+        return new Date(date*1000).toLocaleString('ru', dateOptions.time);
+    }
+}
 
+function transformDegreesToCardinalPoints(deg){
+    if(deg>337 || deg<=22){
+        return 'N';
+    }
+    else if(deg>22 && deg<=67){
+        return 'NE';
+    }
+    else if(deg>67 && deg<=112){
+        return 'E';
+    }
+    else if(deg>112 && deg<=157){
+        return 'SE';
+    }
+    else if(deg>157 && deg<=202){
+        return 'S';
+    }
+    else if(deg>202 && deg<=247){
+        return 'SW';
+    }
+    else if(deg>247 && deg<=292){
+        return 'W'
+    }
+    else if(deg>292 && deg<=337){
+        return 'NW'
+    }
+}
 
+function getLocation(e){
+    let theam = JSON.parse(localStorage.getItem('theam'));
+    console.log(theam);
+    if(theam){
+        main.querySelector(`#${theam.selectBtn}`).classList.add('theam-active');
+        style.href = theam.href;
+    }
+    else{
+        main.querySelector('#dayTheam').classList.add('theam-active')
+    }
 
+    function success(position) {
+        let latitude = position.coords.latitude;
+        let longitude = position.coords.longitude;
+        let altitude = position.coords.altitude;
+        let speed = position.coords.speed;
+        if(latitude && longitude){
+            searchCityInput.value = `${latitude}, ${longitude}`;
+            getCity(e);
+        }
+    };
+     
+    function error(obj) {
+        searchCityInput.value = `Kiev`;
+        getCity(e)
+    };
+
+    let geo = navigator.geolocation.getCurrentPosition(success, error);
+}
 
 
 function switchCountDay(e){
@@ -34,6 +120,41 @@ function switchCountDay(e){
     };
 };
 
+function switchTheam(e){
+    e.preventDefault();
+    let myTarget = e.target.closest('.theam');
+    let activeTheam = main.querySelector('.theam-active');
+    if (myTarget){
+        let theamValue = myTarget.getAttribute('data-theam');
+        let dataTheam = {
+            selectBtn: myTarget.id,
+        }
+        if(activeTheam){
+            activeTheam.classList.remove('theam-active');
+        }
+
+        myTarget.classList.add('theam-active');
+        if(theamValue === 'day'){
+            style.href = 'css/style.css';
+            dataTheam.href = 'css/style.css';
+            localStorage.setItem('theam', JSON.stringify(dataTheam));
+        }
+        else {
+            style.href = 'css/styleDark.css';
+            dataTheam.href = 'css/styleDark.css';
+            localStorage.setItem('theam', JSON.stringify(dataTheam));
+        }
+    }
+}
+
+
+function changeCity(e){
+    let myTarget = e.target.closest('.item');
+    if(myTarget){
+       searchCityInput.value = myTarget.getAttribute('data-cityTitle');
+       getCity(e);
+    }
+}
 
 function getPressKey(e){
     if(e.code === 'Enter'){
@@ -41,16 +162,153 @@ function getPressKey(e){
     }
 }
 
+function showCityInCircle(data){
+    let weatherBlock = main.querySelector('#weather .container');
+    let citysBlock = main.querySelector('.citys-block');
 
-function showTodayWeather(data){
-    console.log(data);
+    if(!citysBlock){
+        citysBlock = document.createElement('section');
+        citysBlock.classList.add('citys-block');
+    }
+    
+    citysBlock.innerHTML = `
+        <h2>Nearby places</h2>
+        <div class="grid">
+            <div class="item" data-cityTitle="${data.list[1].name}">
+                <div class="city-title">${data.list[1].name}</div>
+                <div class="city-weather">
+                    <div>
+                        <img src="http://openweathermap.org/img/wn/${data.list[1].weather[0].icon}.png">
+                    </div>
+                    <p>${Math.round(data.list[1].main.temp)}&#176;C</p>
+                </div>
+            </div>
+
+            <div class="item" data-cityTitle="${data.list[2].name}">
+                <div class="city-title">${data.list[2].name}</div>
+                <div class="city-weather">
+                    <div>
+                        <img src="http://openweathermap.org/img/wn/${data.list[2].weather[0].icon}.png">
+                    </div>
+                    <p>${Math.round(data.list[2].main.temp)}&#176;C</p>
+                </div>
+            </div>
+
+            <div class="item" data-cityTitle="${data.list[3].name}">
+                <div class="city-title">${data.list[3].name}</div>
+                <div class="city-weather">
+                    <div>
+                        <img src="http://openweathermap.org/img/wn/${data.list[3].weather[0].icon}.png">
+                    </div>
+                    <p>${Math.round(data.list[3].main.temp)}&#176;C</p>
+                </div>
+            </div>
+
+            <div class="item" data-cityTitle="${data.list[4].name}">
+                <div class="city-title">${data.list[4].name}</div>
+                <div class="city-weather">
+                    <div>
+                        <img src="http://openweathermap.org/img/wn/${data.list[4].weather[0].icon}.png">
+                    </div>
+                    <p>${Math.round(data.list[4].main.temp)}&#176;C</p>
+                </div>
+            </div>
+        </div>
+    `;
+    citysBlock.addEventListener('click', changeCity);
+    weatherBlock.append(citysBlock);
 }
 
 
-function getTodayWeather(data){
+function showTodayWeather(data){
+    // console.log(data);
+    coords = [data.city.coord.lat, data.city.coord.lon];
+    searchCityInput.value = `${data.city.name}, ${data.city.country}`
+    let weatherBlock = main.querySelector('#weather');
+    let currentWeatherBlock = main.querySelector('.current-weather');
+    let hourlyWeather = main.querySelector('.hourly-weather')
+    let todayDate = new Date().toLocaleDateString('ru', dateOptions.currentDate);
+    if(!weatherBlock){
+        weatherBlock = document.createElement('div');
+        weatherBlock.id = 'weather';
+        weatherBlock.classList.add('weather-block');
+
+        currentWeatherBlock = document.createElement('section');
+        currentWeatherBlock.classList.add('current-weather');
+        hourlyWeather = document.createElement('section');
+        hourlyWeather.classList.add('hourly-weather')
+
+        main.append(weatherBlock);
+    }
+
+    currentWeatherBlock.innerHTML = `
+        <h2 class="today-date">Погода сейчас <span>${todayDate}</span></h2>
+        <div class="flex">
+            <div class="item">
+                <img src="http://openweathermap.org/img/wn/${data.list['0'].weather[0].icon}.png" class="weather-icon">
+                <p>${data.list['0'].weather[0].description}</p>
+            </div>
+            <div class="item">
+                <p class="current-temp">${Math.round(data.list['0'].main.temp)}&#176;C</p>
+                <p>Real feel ${Math.round(data.list['0'].main['feels_like'])}&#176;C</p>
+                <p></p>
+            </div>
+            <div class="item">
+                <p>Sunrise: ${transormUNIX(data.city.sunrise,'no-add')}</p>
+                <p>Sunset: ${transormUNIX(data.city.sunset,'no-add')}</p>
+            </div>
+        </div>
+    `;
+
+    let fiveHours = data.list.slice(0,5);
+    let hourWeather = fiveHours.map(item =>`
+        <div class="item">
+            <p class="time">${transormUNIX(item.dt)}</p>
+            <div class="hour-weather-icon">
+                <img src="http://openweathermap.org/img/wn/${item.weather[0].icon}.png">
+            </div>
+            <p>${item.weather[0].main}</p>
+            <p>${Math.round(item.main.temp)}&#176;</p>
+            <p>${Math.round(item.main['feels_like'])}&#176;</p>
+            <p>${Math.round(item.wind.speed)} ${transformDegreesToCardinalPoints(item.wind.deg)}</p>  
+        </div>
+    `);
+
+    hourlyWeather.innerHTML = `
+        <h2>Hourly</h2>
+        <div class="flex">
+            <div class="item info">
+                <p class="time"><span>Today</span></p>
+                <div class="hour-weather-icon"></div>
+                <p>Forecast</p>
+                <p>Temp (&#176;C)</p>
+                <p>RealFeel</p>
+                <p>Wind (km/h)</p>
+            </div>
+            ${hourWeather.join('')}
+        </div>
+    `;
+
+    weatherBlock.innerHTML = `
+    <div class="container">
+        ${currentWeatherBlock.outerHTML}
+        ${hourlyWeather.outerHTML}
+    </div>
+`;
+
+    fetchAsync(`https://api.openweathermap.org/data/2.5/find?lat=${coords[0]}&lon=${coords[1]}&cnt=5&lang=en&units=metric&appid=2e45c48feaeca4beaf24076750d9e0c7`)
+    .then(data => getTodayWeather(data));
+}
+
+
+async function getTodayWeather(data){
     let errorImg = main.querySelector('.error-img');
+    // console.log(data)
 
     if(data.cod >= 400 && !errorImg){
+        if(main.querySelector('#weather')){
+            main.querySelector('#weather').remove()
+        }
         errorImg = document.createElement('img');
         errorImg.src = 'img/404_error.png';
         errorImg.classList.add('error-img');
@@ -60,7 +318,12 @@ function getTodayWeather(data){
         if(errorImg){
             errorImg.remove();
         };
-        showTodayWeather(data);
+         if(data.count === 5){
+            showCityInCircle(data)
+         }
+         else{
+            showTodayWeather(data);
+         }
     };
 };
 
@@ -72,8 +335,20 @@ function getCity(e){
     searchCityInput.value = searchCityInput.value.trim();
 
     if(countDays === '1'){
-        fetchAsync(`https://api.openweathermap.org/data/2.5/weather?q=${searchCityInput.value}&lang=ru&appid=2e45c48feaeca4beaf24076750d9e0c7`)
-        .then(data => getTodayWeather(data));
+        //Поиск по названию города
+        if(regEx.cityByName.test(searchCityInput.value) === true){
+            searchCityInput.value = searchCityInput.value.replace(/\s+/g, ' ');
+            console.log(searchCityInput.value)
+            fetchAsync(`https://api.openweathermap.org/data/2.5/forecast?q=${searchCityInput.value}&lang=en&units=metric&appid=2e45c48feaeca4beaf24076750d9e0c7`)
+            .then(data => getTodayWeather(data));
+        }
+        //Поиск по кординатам
+        else if(regEx.cityByName.test(searchCityInput.value) === false){
+            searchCityInput.value = searchCityInput.value.replace(/\s+/g, '')
+            coords = searchCityInput.value.split(',');
+            fetchAsync(`https://api.openweathermap.org/data/2.5/forecast?lat=${coords[0]}&lon=${coords[1]}&lang=en&units=metric&appid=2e45c48feaeca4beaf24076750d9e0c7`)
+            .then(data => getTodayWeather(data));
+        }
     }
     else if(countDays === '5'){
         console.log('five days');
@@ -95,10 +370,14 @@ searchCityInput.addEventListener('keydown', getPressKey);
 //Переключение количества дней
 switchBlock.addEventListener('click', switchCountDay);
 
-window.addEventListener('load', getCity)
+switchTheamBlock.addEventListener('click', switchTheam);
+
+//Определить место положение пользователя при запуске
+window.addEventListener('load', getLocation);
 
 
 
 
 
 // const myUrlToday = `https://api.openweathermap.org/data/2.5/weather?q=${searchCityInput.value}&lang=ru&appid=2e45c48feaeca4beaf24076750d9e0c7`;
+
